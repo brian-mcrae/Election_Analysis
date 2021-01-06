31ac4d314eda587e6097b79e4f4db2655480b90f
Brian McRae <brian.mcrae@red-gate.com>
2021-01-06 14:19:08 -0600

    added write to file logic

--- PyPoll.py
+++ PyPoll.py
@@ -20,3 +20,23 @@ with open(file_to_load) as election_data:
 
 # Close the file.
 election_data.close()
+
+# Create a filename variable to direct or indirect path to the file
+file_to_save = os.path.join("Analysis","election_analysis.txt")
+
+# Using the open() function with the "w" mode we will write data to the file
+# open(file_to_save, "w")
+# outfile = open(file_to_save,"w")
+with open(file_to_save,"w") as txt_file:
+
+    # Write some data to the file
+    # outfile.write("Hello World")
+    # txt_file.write("Hello World")
+
+    # Write three counties to the file
+    txt_file.write("Arapahoe, ")
+    txt_file.write("Denver, ")
+    txt_file.write("Jefferson")
+
+# Close the file
+# outfile.close()
