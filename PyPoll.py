12cbb67dbf140fff22f475b8c83bf976d701a83e
Brian McRae <brian.mcrae@red-gate.com>
2021-01-06 14:54:11 -0600

    Completed file data access

--- PyPoll.py
+++ PyPoll.py
@@ -17,4 +17,9 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: read and analyze the data here.
+    # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+    
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
