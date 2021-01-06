7c56599e36974d7f0e7af7fd4d26e582da335e18
Brian McRae <brian.mcrae@red-gate.com>
2021-01-06 16:15:02 -0600

    populated candidate options list

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,12 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter
+total_votes = 0
+
+# Initialize Candidate List
+candidate_options = []
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -22,4 +28,23 @@ with open(file_to_load) as election_data:
     
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+    
+    # Print each row in the CSV file
+    for row in file_reader:
+        
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+        
+            # Add the candidate name to the candidate options list
+            candidate_options.append(candidate_name)
+    
+        # Add to the toal vote count
+        total_votes += 1
+
+# Print the candidate options list
+print(candidate_options)
+
+# Print the total votes
+print(total_votes)
